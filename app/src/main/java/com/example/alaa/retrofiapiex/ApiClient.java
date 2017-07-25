fc56bfde99c6b7176e648bbdcf367aaec2a8c2e2
Alaa <alaamimi@hotmail.com>
2017-07-25 17:46:10 +0200

    test

--- app/src/main/java/com/example/alaa/retrofiapiex/ApiClient.java
+++ app/src/main/java/com/example/alaa/retrofiapiex/ApiClient.java
@@ -8,15 +8,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class ApiClient {
-    public static final String Base_URL="http://192.168.1.107/";
-    public  static Retrofit retrofit;
+    public static final String Base_URL = "http://192.168.1.107/";
+    public static Retrofit retrofit,retrofit2;
 
-    public  static Retrofit getApiClient()
-    {
+    public static Retrofit getApiClient() {
 
-        if(retrofit==null)
-        {
-            retrofit=new Retrofit.Builder().baseUrl(Base_URL).
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder().baseUrl(Base_URL).
                     addConverterFactory(GsonConverterFactory.create()).build();
         }
         return retrofit;
